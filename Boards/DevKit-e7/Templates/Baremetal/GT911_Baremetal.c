5350d936acd8f15cff74ad2f1c6aa4cd2732800e
Chandra Bhushan Singh <chandrabhushan.singh@alifsemi.com>
2025-04-11 12:47:18 +0530

    boards: devkit-e7: GT911: Resolve random coordinate generation on initial touch
    Addressed the issue of random coordinate values being generated for additional
    touch points, even when only a single touch point was engaged on the GT911
    touch screen.
    
    Signed-off-by: Chandra Bhushan Singh <chandrabhushan.singh@alifsemi.com>

--- Boards/DevKit-e7/Templates/Baremetal/GT911_Baremetal.c
+++ Boards/DevKit-e7/Templates/Baremetal/GT911_Baremetal.c
@@ -175,6 +175,9 @@ void touchscreen_demo()
                 printf("x%d: %d y%d: %d \r\n",count, state.coordinates[count - 1].x, count, state.coordinates[count - 1].y);
             }
             memset(state.coordinates,0,sizeof(state.coordinates));
+        } else{
+            /* Clear coordinates when no touch is detected to avoid printing old data */
+            memset(state.coordinates, 0, sizeof(state.coordinates));
         }
     }
 
