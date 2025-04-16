d492bb6a5f4aad49d7b556314e3efd01c020f14d
Manoj A Murudi <manoj.murudi@alifsemi.com>
2025-04-16 16:55:15 +0530

    gpio: add support to E1C family
    added support to E1C family
    
    Signed-off-by: Manoj A Murudi <manoj.murudi@alifsemi.com>

--- Alif_CMSIS/Source/Driver_GPIO.c
+++ Alif_CMSIS/Source/Driver_GPIO.c
@@ -18,69 +18,126 @@
  * $Date:        16. July 2024
  * $Revision:    V1.0
  *
- * Project:      GPIO Driver for Alif Semiconductor Ensemble
+ * Project:      GPIO Driver for Alif Semiconductor
  */
 
 #include "Driver_GPIO_Private.h"
 
 #include "pinconf.h"
 #include "gpio.h"
+#include "soc_features.h"
+#include "RTE_Device.h"
 
 #if defined(RTE_Drivers_GPIO)
 
-// Pin mapping
-//   0 ..   7: P0_0..7  (GPIO0)
-//   8 ..  15: P1_0..7  (GPIO1)
-//  16 ..  23: P2_0..7  (GPIO2)
-//  24 ..  31: P3_0..7  (GPIO3)
-//  32 ..  39: P4_0..7  (GPIO4)
-//  40 ..  47: P5_0..7  (GPIO5)
-//  48 ..  55: P6_0..7  (GPIO6)
-//  56 ..  63: P7_0..7  (GPIO7)
-//  64 ..  71: P8_0..7  (GPIO8)
-//  72 ..  79: P9_0..7  (GPIO9)
-//  80 ..  87: P10_0..7 (GPIO10)
-//  88 ..  95: P11_0..7 (GPIO11)
-//  96 .. 103: P12_0..7 (GPIO12)
-// 104 .. 111: P13_0..7 (GPIO13)
-// 112 .. 119: P14_0..7 (GPIO14)
-// 120 .. 127: P15_0..7 (LPGPIO)
+/* Pin mapping for Ensemble Ex family
+   0 ..   7: P0_0..7  (GPIO0)
+   8 ..  15: P1_0..7  (GPIO1)
+  16 ..  23: P2_0..7  (GPIO2)
+  24 ..  31: P3_0..7  (GPIO3)
+  32 ..  39: P4_0..7  (GPIO4)
+  40 ..  47: P5_0..7  (GPIO5)
+  48 ..  55: P6_0..7  (GPIO6)
+  56 ..  63: P7_0..7  (GPIO7)
+  64 ..  71: P8_0..7  (GPIO8)
+  72 ..  79: P9_0..7  (GPIO9)
+  80 ..  87: P10_0..7 (GPIO10)
+  88 ..  95: P11_0..7 (GPIO11)
+  96 .. 103: P12_0..7 (GPIO12)
+ 104 .. 111: P13_0..7 (GPIO13)
+ 112 .. 119: P14_0..7 (GPIO14)
+ 120 .. 127: P15_0..7 (LPGPIO) */
 
+/* Pin mapping for Ensemble E1C family
+   0 ..   7: P0_0..7  (GPIO0)
+   8 ..  15: P1_0..7  (GPIO1)
+  16 ..  23: P2_0..7  (GPIO2)
+  24 ..  31: P3_0..7  (GPIO3)
+  32 ..  39: P4_0..7  (GPIO4)
+  40 ..  47: P5_0..7  (GPIO5)
+  48 ..  55: P6_0..7  (GPIO6)
+  56 ..  63: P7_0..7  (GPIO7)
+  64 ..  71: P8_0..7  (GPIO8)
+  72 ..  79: P9_0..7  (GPIO9)
+ 120 .. 121: P15_0..1 (LPGPIO) */
 
-#define GPIO_MAX_PORTS          16U
-#define GPIO_MAX_PINS           128U
+#define GPIO_MAX_PINS    8U
+#if (SOC_FEAT_LPGPIO_HAS_PIN2_7)
+#define LPGPIO_MAX_PINS  8U
+#else
+#define LPGPIO_MAX_PINS  2U
+#endif
 
-// GPIOx IRQ Numbers
-static IRQn_Type const GPIO_IRQn[GPIO_MAX_PORTS] = {
-  GPIO0_IRQ0_IRQn,  GPIO1_IRQ0_IRQn,  GPIO2_IRQ0_IRQn,  GPIO3_IRQ0_IRQn,
-  GPIO4_IRQ0_IRQn,  GPIO5_IRQ0_IRQn,  GPIO6_IRQ0_IRQn,  GPIO7_IRQ0_IRQn,
-  GPIO8_IRQ0_IRQn,  GPIO9_IRQ0_IRQn,  GPIO10_IRQ0_IRQn, GPIO11_IRQ0_IRQn,
-  GPIO12_IRQ0_IRQn, GPIO13_IRQ0_IRQn, GPIO14_IRQ0_IRQn, LPGPIO_IRQ0_IRQn
+/* GPIO Resources struct */
+typedef struct _GPIO_RESOURCES {
+  const uint32_t         base;                     /**< GPIO PORT Base Address>**/
+  const IRQn_Type        irqn;                     /**< GPIO PORT IRQ base Num>**/
+  const uint8_t          max_pins;                 /**< GPIO PORT Pin Num>**/
+  ARM_GPIO_SignalEvent_t cb_event[GPIO_MAX_PINS];  /**< GPIO Signal Event callback functions>**/
+} GPIO_RESOURCES;
+
+/* GPIO_RESOURCES array for each GPIO port */
+static GPIO_RESOURCES gpio_res[] = {
+#if (RTE_GPIO0)
+  { GPIO0_BASE, GPIO0_IRQ0_IRQn, GPIO_MAX_PINS, NULL },
+#endif
+#if (RTE_GPIO1)
+  { GPIO1_BASE, GPIO1_IRQ0_IRQn, GPIO_MAX_PINS, NULL },
+#endif
+#if (RTE_GPIO2)
+  { GPIO2_BASE, GPIO2_IRQ0_IRQn, GPIO_MAX_PINS, NULL },
+#endif
+#if (RTE_GPIO3)
+  { GPIO3_BASE, GPIO3_IRQ0_IRQn, GPIO_MAX_PINS, NULL },
+#endif
+#if (RTE_GPIO4)
+  { GPIO4_BASE, GPIO4_IRQ0_IRQn, GPIO_MAX_PINS, NULL },
+#endif
+#if (RTE_GPIO5)
+  { GPIO5_BASE, GPIO5_IRQ0_IRQn, GPIO_MAX_PINS, NULL },
+#endif
+#if (RTE_GPIO6)
+  { GPIO6_BASE, GPIO6_IRQ0_IRQn, GPIO_MAX_PINS, NULL },
+#endif
+#if (RTE_GPIO7)
+  { GPIO7_BASE, GPIO7_IRQ0_IRQn, GPIO_MAX_PINS, NULL },
+#endif
+#if (RTE_GPIO8)
+  { GPIO8_BASE, GPIO8_IRQ0_IRQn, GPIO_MAX_PINS, NULL },
+#endif
+#if (RTE_GPIO9)
+  { GPIO9_BASE, GPIO9_IRQ0_IRQn, GPIO_MAX_PINS, NULL },
+#endif
+#if (RTE_GPIO10)
+  { GPIO10_BASE, GPIO10_IRQ0_IRQn, GPIO_MAX_PINS, NULL },
+#endif
+#if (RTE_GPIO11)
+  { GPIO11_BASE, GPIO11_IRQ0_IRQn, GPIO_MAX_PINS, NULL },
+#endif
+#if (RTE_GPIO12)
+  { GPIO12_BASE, GPIO12_IRQ0_IRQn, GPIO_MAX_PINS, NULL },
+#endif
+#if (RTE_GPIO13)
+  { GPIO13_BASE, GPIO13_IRQ0_IRQn, GPIO_MAX_PINS, NULL },
+#endif
+#if (RTE_GPIO14)
+  { GPIO14_BASE, GPIO14_IRQ0_IRQn, GPIO_MAX_PINS, NULL },
+#endif
+#if (RTE_LPGPIO)
+  { LPGPIO_BASE, LPGPIO_IRQ0_IRQn, LPGPIO_MAX_PINS, NULL },
+#endif
 };
 
-// GPIOx Base Pointers
-static uint32_t const GPIO_Base[GPIO_MAX_PORTS] = {
-  GPIO0_BASE,  GPIO1_BASE,  GPIO2_BASE,  GPIO3_BASE,
-  GPIO4_BASE,  GPIO5_BASE,  GPIO6_BASE,  GPIO7_BASE,
-  GPIO8_BASE,  GPIO9_BASE,  GPIO10_BASE, GPIO11_BASE,
-  GPIO12_BASE, GPIO13_BASE, GPIO14_BASE, LPGPIO_BASE
-};
-
-
-// Signal Event callback functions
-static ARM_GPIO_SignalEvent_t SignalEvent[GPIO_MAX_PORTS][8];
-
-
-// Common GPIO IRQ Handler
+/* Common GPIO IRQ Handler */
 static void GPIO_IRQHandler (uint32_t gpio_num, uint32_t pin_num) {
-  GPIO_Type *gpio = (GPIO_Type *)GPIO_Base[gpio_num];
+  GPIO_Type *gpio = (GPIO_Type *)gpio_res[gpio_num].base;
   ARM_GPIO_Pin_t pin;
   uint32_t event;
 
   /* Clear pin interrupt */
   gpio_interrupt_eoi(gpio, pin_num);
 
-  if (SignalEvent[gpio_num][pin_num] != NULL) {
+  if (gpio_res[gpio_num].cb_event[pin_num] != NULL) {
     pin = (8U * gpio_num) + pin_num;
 
     /* Determine interrupt polarity/edge */
@@ -91,10 +148,11 @@ static void GPIO_IRQHandler (uint32_t gpio_num, uint32_t pin_num) {
       event = ARM_GPIO_EVENT_FALLING_EDGE;
     }
 
-    SignalEvent[gpio_num][pin_num](pin, event);
+    gpio_res[gpio_num].cb_event[pin_num](pin, event);
   }
 }
 
+#if (RTE_GPIO0)
 void GPIO0_IRQ0Handler (void){ GPIO_IRQHandler(0U, 0U); }
 void GPIO0_IRQ1Handler (void){ GPIO_IRQHandler(0U, 1U); }
 void GPIO0_IRQ2Handler (void){ GPIO_IRQHandler(0U, 2U); }
@@ -103,7 +161,9 @@ void GPIO0_IRQ4Handler (void){ GPIO_IRQHandler(0U, 4U); }
 void GPIO0_IRQ5Handler (void){ GPIO_IRQHandler(0U, 5U); }
 void GPIO0_IRQ6Handler (void){ GPIO_IRQHandler(0U, 6U); }
 void GPIO0_IRQ7Handler (void){ GPIO_IRQHandler(0U, 7U); }
+#endif
 
+#if (RTE_GPIO1)
 void GPIO1_IRQ0Handler (void){ GPIO_IRQHandler(1U, 0U); }
 void GPIO1_IRQ1Handler (void){ GPIO_IRQHandler(1U, 1U); }
 void GPIO1_IRQ2Handler (void){ GPIO_IRQHandler(1U, 2U); }
@@ -112,7 +172,9 @@ void GPIO1_IRQ4Handler (void){ GPIO_IRQHandler(1U, 4U); }
 void GPIO1_IRQ5Handler (void){ GPIO_IRQHandler(1U, 5U); }
 void GPIO1_IRQ6Handler (void){ GPIO_IRQHandler(1U, 6U); }
 void GPIO1_IRQ7Handler (void){ GPIO_IRQHandler(1U, 7U); }
+#endif
 
+#if (RTE_GPIO2)
 void GPIO2_IRQ0Handler (void){ GPIO_IRQHandler(2U, 0U); }
 void GPIO2_IRQ1Handler (void){ GPIO_IRQHandler(2U, 1U); }
 void GPIO2_IRQ2Handler (void){ GPIO_IRQHandler(2U, 2U); }
@@ -121,7 +183,9 @@ void GPIO2_IRQ4Handler (void){ GPIO_IRQHandler(2U, 4U); }
 void GPIO2_IRQ5Handler (void){ GPIO_IRQHandler(2U, 5U); }
 void GPIO2_IRQ6Handler (void){ GPIO_IRQHandler(2U, 6U); }
 void GPIO2_IRQ7Handler (void){ GPIO_IRQHandler(2U, 7U); }
+#endif
 
+#if (RTE_GPIO3)
 void GPIO3_IRQ0Handler (void){ GPIO_IRQHandler(3U, 0U); }
 void GPIO3_IRQ1Handler (void){ GPIO_IRQHandler(3U, 1U); }
 void GPIO3_IRQ2Handler (void){ GPIO_IRQHandler(3U, 2U); }
@@ -130,7 +194,9 @@ void GPIO3_IRQ4Handler (void){ GPIO_IRQHandler(3U, 4U); }
 void GPIO3_IRQ5Handler (void){ GPIO_IRQHandler(3U, 5U); }
 void GPIO3_IRQ6Handler (void){ GPIO_IRQHandler(3U, 6U); }
 void GPIO3_IRQ7Handler (void){ GPIO_IRQHandler(3U, 7U); }
+#endif
 
+#if (RTE_GPIO4)
 void GPIO4_IRQ0Handler (void){ GPIO_IRQHandler(4U, 0U); }
 void GPIO4_IRQ1Handler (void){ GPIO_IRQHandler(4U, 1U); }
 void GPIO4_IRQ2Handler (void){ GPIO_IRQHandler(4U, 2U); }
@@ -139,7 +205,9 @@ void GPIO4_IRQ4Handler (void){ GPIO_IRQHandler(4U, 4U); }
 void GPIO4_IRQ5Handler (void){ GPIO_IRQHandler(4U, 5U); }
 void GPIO4_IRQ6Handler (void){ GPIO_IRQHandler(4U, 6U); }
 void GPIO4_IRQ7Handler (void){ GPIO_IRQHandler(4U, 7U); }
+#endif
 
+#if (RTE_GPIO5)
 void GPIO5_IRQ0Handler (void){ GPIO_IRQHandler(5U, 0U); }
 void GPIO5_IRQ1Handler (void){ GPIO_IRQHandler(5U, 1U); }
 void GPIO5_IRQ2Handler (void){ GPIO_IRQHandler(5U, 2U); }
@@ -148,7 +216,9 @@ void GPIO5_IRQ4Handler (void){ GPIO_IRQHandler(5U, 4U); }
 void GPIO5_IRQ5Handler (void){ GPIO_IRQHandler(5U, 5U); }
 void GPIO5_IRQ6Handler (void){ GPIO_IRQHandler(5U, 6U); }
 void GPIO5_IRQ7Handler (void){ GPIO_IRQHandler(5U, 7U); }
+#endif
 
+#if (RTE_GPIO6)
 void GPIO6_IRQ0Handler (void){ GPIO_IRQHandler(6U, 0U); }
 void GPIO6_IRQ1Handler (void){ GPIO_IRQHandler(6U, 1U); }
 void GPIO6_IRQ2Handler (void){ GPIO_IRQHandler(6U, 2U); }
@@ -157,7 +227,9 @@ void GPIO6_IRQ4Handler (void){ GPIO_IRQHandler(6U, 4U); }
 void GPIO6_IRQ5Handler (void){ GPIO_IRQHandler(6U, 5U); }
 void GPIO6_IRQ6Handler (void){ GPIO_IRQHandler(6U, 6U); }
 void GPIO6_IRQ7Handler (void){ GPIO_IRQHandler(6U, 7U); }
+#endif
 
+#if (RTE_GPIO7)
 void GPIO7_IRQ0Handler (void){ GPIO_IRQHandler(7U, 0U); }
 void GPIO7_IRQ1Handler (void){ GPIO_IRQHandler(7U, 1U); }
 void GPIO7_IRQ2Handler (void){ GPIO_IRQHandler(7U, 2U); }
@@ -166,7 +238,9 @@ void GPIO7_IRQ4Handler (void){ GPIO_IRQHandler(7U, 4U); }
 void GPIO7_IRQ5Handler (void){ GPIO_IRQHandler(7U, 5U); }
 void GPIO7_IRQ6Handler (void){ GPIO_IRQHandler(7U, 6U); }
 void GPIO7_IRQ7Handler (void){ GPIO_IRQHandler(7U, 7U); }
+#endif
 
+#if (RTE_GPIO8)
 void GPIO8_IRQ0Handler (void){ GPIO_IRQHandler(8U, 0U); }
 void GPIO8_IRQ1Handler (void){ GPIO_IRQHandler(8U, 1U); }
 void GPIO8_IRQ2Handler (void){ GPIO_IRQHandler(8U, 2U); }
@@ -175,7 +249,9 @@ void GPIO8_IRQ4Handler (void){ GPIO_IRQHandler(8U, 4U); }
 void GPIO8_IRQ5Handler (void){ GPIO_IRQHandler(8U, 5U); }
 void GPIO8_IRQ6Handler (void){ GPIO_IRQHandler(8U, 6U); }
 void GPIO8_IRQ7Handler (void){ GPIO_IRQHandler(8U, 7U); }
+#endif
 
+#if (RTE_GPIO9)
 void GPIO9_IRQ0Handler (void){ GPIO_IRQHandler(9U, 0U); }
 void GPIO9_IRQ1Handler (void){ GPIO_IRQHandler(9U, 1U); }
 void GPIO9_IRQ2Handler (void){ GPIO_IRQHandler(9U, 2U); }
@@ -184,7 +260,9 @@ void GPIO9_IRQ4Handler (void){ GPIO_IRQHandler(9U, 4U); }
 void GPIO9_IRQ5Handler (void){ GPIO_IRQHandler(9U, 5U); }
 void GPIO9_IRQ6Handler (void){ GPIO_IRQHandler(9U, 6U); }
 void GPIO9_IRQ7Handler (void){ GPIO_IRQHandler(9U, 7U); }
+#endif
 
+#if (RTE_GPIO10)
 void GPIO10_IRQ0Handler (void){ GPIO_IRQHandler(10U, 0U); }
 void GPIO10_IRQ1Handler (void){ GPIO_IRQHandler(10U, 1U); }
 void GPIO10_IRQ2Handler (void){ GPIO_IRQHandler(10U, 2U); }
@@ -193,7 +271,9 @@ void GPIO10_IRQ4Handler (void){ GPIO_IRQHandler(10U, 4U); }
 void GPIO10_IRQ5Handler (void){ GPIO_IRQHandler(10U, 5U); }
 void GPIO10_IRQ6Handler (void){ GPIO_IRQHandler(10U, 6U); }
 void GPIO10_IRQ7Handler (void){ GPIO_IRQHandler(10U, 7U); }
+#endif
 
+#if (RTE_GPIO11)
 void GPIO11_IRQ0Handler (void){ GPIO_IRQHandler(11U, 0U); }
 void GPIO11_IRQ1Handler (void){ GPIO_IRQHandler(11U, 1U); }
 void GPIO11_IRQ2Handler (void){ GPIO_IRQHandler(11U, 2U); }
@@ -202,7 +282,9 @@ void GPIO11_IRQ4Handler (void){ GPIO_IRQHandler(11U, 4U); }
 void GPIO11_IRQ5Handler (void){ GPIO_IRQHandler(11U, 5U); }
 void GPIO11_IRQ6Handler (void){ GPIO_IRQHandler(11U, 6U); }
 void GPIO11_IRQ7Handler (void){ GPIO_IRQHandler(11U, 7U); }
+#endif
 
+#if (RTE_GPIO12)
 void GPIO12_IRQ0Handler (void){ GPIO_IRQHandler(12U, 0U); }
 void GPIO12_IRQ1Handler (void){ GPIO_IRQHandler(12U, 1U); }
 void GPIO12_IRQ2Handler (void){ GPIO_IRQHandler(12U, 2U); }
@@ -211,7 +293,9 @@ void GPIO12_IRQ4Handler (void){ GPIO_IRQHandler(12U, 4U); }
 void GPIO12_IRQ5Handler (void){ GPIO_IRQHandler(12U, 5U); }
 void GPIO12_IRQ6Handler (void){ GPIO_IRQHandler(12U, 6U); }
 void GPIO12_IRQ7Handler (void){ GPIO_IRQHandler(12U, 7U); }
+#endif
 
+#if (RTE_GPIO13)
 void GPIO13_IRQ0Handler (void){ GPIO_IRQHandler(13U, 0U); }
 void GPIO13_IRQ1Handler (void){ GPIO_IRQHandler(13U, 1U); }
 void GPIO13_IRQ2Handler (void){ GPIO_IRQHandler(13U, 2U); }
@@ -220,7 +304,9 @@ void GPIO13_IRQ4Handler (void){ GPIO_IRQHandler(13U, 4U); }
 void GPIO13_IRQ5Handler (void){ GPIO_IRQHandler(13U, 5U); }
 void GPIO13_IRQ6Handler (void){ GPIO_IRQHandler(13U, 6U); }
 void GPIO13_IRQ7Handler (void){ GPIO_IRQHandler(13U, 7U); }
+#endif
 
+#if (RTE_GPIO14)
 void GPIO14_IRQ0Handler (void){ GPIO_IRQHandler(14U, 0U); }
 void GPIO14_IRQ1Handler (void){ GPIO_IRQHandler(14U, 1U); }
 void GPIO14_IRQ2Handler (void){ GPIO_IRQHandler(14U, 2U); }
@@ -229,30 +315,36 @@ void GPIO14_IRQ4Handler (void){ GPIO_IRQHandler(14U, 4U); }
 void GPIO14_IRQ5Handler (void){ GPIO_IRQHandler(14U, 5U); }
 void GPIO14_IRQ6Handler (void){ GPIO_IRQHandler(14U, 6U); }
 void GPIO14_IRQ7Handler (void){ GPIO_IRQHandler(14U, 7U); }
+#endif
 
+#if (RTE_LPGPIO)
 void LPGPIO_IRQ0Handler (void){ GPIO_IRQHandler(15U, 0U); }
 void LPGPIO_IRQ1Handler (void){ GPIO_IRQHandler(15U, 1U); }
+#if (SOC_FEAT_LPGPIO_HAS_PIN2_7)
 void LPGPIO_IRQ2Handler (void){ GPIO_IRQHandler(15U, 2U); }
 void LPGPIO_IRQ3Handler (void){ GPIO_IRQHandler(15U, 3U); }
 void LPGPIO_IRQ4Handler (void){ GPIO_IRQHandler(15U, 4U); }
 void LPGPIO_IRQ5Handler (void){ GPIO_IRQHandler(15U, 5U); }
 void LPGPIO_IRQ6Handler (void){ GPIO_IRQHandler(15U, 6U); }
 void LPGPIO_IRQ7Handler (void){ GPIO_IRQHandler(15U, 7U); }
+#endif
+#endif
 
-
-// Setup GPIO Interface
+/* Setup GPIO Interface */
 static int32_t GPIO_Setup (ARM_GPIO_Pin_t pin, ARM_GPIO_SignalEvent_t cb_event) {
   GPIO_Type *gpio;
   uint32_t   gpio_num;
   uint32_t   pin_num;
   int32_t    status;
 
-  if (pin >= GPIO_MAX_PINS) {
+  gpio_num = pin >> 3U;
+  pin_num  = pin & 0x7U;
+
+  if (pin_num >= gpio_res[gpio_num].max_pins) {
     status = ARM_GPIO_ERROR_PIN;
   }
   else {
-    gpio_num = pin >> 3U;
-    pin_num  = pin & 0x7U;
+    status = ARM_DRIVER_OK;
 
     /* Setup I/O signal multiplexer */
     pinconf_set(gpio_num, pin_num, PINMUX_ALTERNATE_FUNCTION_0, PADCTRL_READ_ENABLE              |
@@ -260,7 +352,7 @@ static int32_t GPIO_Setup (ARM_GPIO_Pin_t pin, ARM_GPIO_SignalEvent_t cb_event) 
                                                                 PADCTRL_DRIVER_DISABLED_HIGH_Z   |
                                                                 PADCTRL_OUTPUT_DRIVE_STRENGTH_2MA);
 
-    gpio = (GPIO_Type *)GPIO_Base[gpio_num];
+    gpio = (GPIO_Type *) gpio_res[gpio_num].base;
 
     /* Default direction is input */
     gpio_set_direction_input(gpio, pin_num);
@@ -271,13 +363,13 @@ static int32_t GPIO_Setup (ARM_GPIO_Pin_t pin, ARM_GPIO_SignalEvent_t cb_event) 
     }
     else {
       /* Save pin callback event */
-      SignalEvent[gpio_num][pin_num] = cb_event;
+      gpio_res[gpio_num].cb_event[pin_num] = cb_event;
 
       /* Enable peripheral interrupt generation */
       gpio_mask_interrupt(gpio, pin_num);
 
       /* Enable NVIC interrupt generation */
-      NVIC_EnableIRQ((IRQn_Type)(GPIO_IRQn[gpio_num] + pin_num));
+      NVIC_EnableIRQ((IRQn_Type)(gpio_res[gpio_num].irqn + pin_num));
 
       /* Enable interrupt */
       gpio_enable_interrupt(gpio, pin_num);
@@ -289,23 +381,23 @@ static int32_t GPIO_Setup (ARM_GPIO_Pin_t pin, ARM_GPIO_SignalEvent_t cb_event) 
   return status;
 }
 
-// Set GPIO Direction
+/* Set GPIO Direction */
 static int32_t GPIO_SetDirection (ARM_GPIO_Pin_t pin, ARM_GPIO_DIRECTION direction) {
   GPIO_Type *gpio;
   uint32_t   gpio_num;
   uint32_t   pin_num;
   int32_t    status;
 
-  if (pin >= GPIO_MAX_PINS) {
+  gpio_num = pin >> 3U;
+  pin_num  = pin & 0x7U;
+
+  if (pin_num >= gpio_res[gpio_num].max_pins) {
     status = ARM_GPIO_ERROR_PIN;
   }
   else {
     status = ARM_DRIVER_OK;
 
-    gpio_num = pin >> 3U;
-    pin_num  = pin & 0x7U;
-
-    gpio = (GPIO_Type *)GPIO_Base[gpio_num];
+    gpio = (GPIO_Type *)gpio_res[gpio_num].base;
 
     if (direction == ARM_GPIO_INPUT) {
       gpio_set_direction_input(gpio, pin_num);
@@ -321,7 +413,7 @@ static int32_t GPIO_SetDirection (ARM_GPIO_Pin_t pin, ARM_GPIO_DIRECTION directi
   return status;
 }
 
-// Set GPIO Output Mode
+/* Set GPIO Output Mode */
 static int32_t GPIO_SetOutputMode (ARM_GPIO_Pin_t pin, ARM_GPIO_OUTPUT_MODE mode) {
   uint32_t   gpio_num;
   uint32_t   pin_num;
@@ -329,14 +421,14 @@ static int32_t GPIO_SetOutputMode (ARM_GPIO_Pin_t pin, ARM_GPIO_OUTPUT_MODE mode
   uint8_t    alt_func;
   uint8_t    pad_ctrl;
 
-  if (pin >= GPIO_MAX_PINS) {
+  gpio_num = pin >> 3U;
+  pin_num  = pin & 0x7U;
+
+  if (pin_num >= gpio_res[gpio_num].max_pins) {
     status = ARM_GPIO_ERROR_PIN;
   }
   else {
     status = ARM_DRIVER_OK;
-
-    gpio_num = pin >> 3U;
-    pin_num  = pin & 0x7U;
 
     /* Read current configuration */
     if (pinconf_get(gpio_num, pin_num, &alt_func, &pad_ctrl) != 0) {
@@ -363,7 +455,7 @@ static int32_t GPIO_SetOutputMode (ARM_GPIO_Pin_t pin, ARM_GPIO_OUTPUT_MODE mode
   return status;
 }
 
-// Set GPIO Pull Resistor
+/* Set GPIO Pull Resistor */
 static int32_t GPIO_SetPullResistor (ARM_GPIO_Pin_t pin, ARM_GPIO_PULL_RESISTOR resistor) {
   uint32_t   gpio_num;
   uint32_t   pin_num;
@@ -371,7 +463,10 @@ static int32_t GPIO_SetPullResistor (ARM_GPIO_Pin_t pin, ARM_GPIO_PULL_RESISTOR 
   uint8_t    alt_func;
   uint8_t    pad_ctrl;
 
-  if (pin >= GPIO_MAX_PINS) {
+  gpio_num = pin >> 3U;
+  pin_num  = pin & 0x7U;
+
+  if (pin_num >= gpio_res[gpio_num].max_pins) {
     status = ARM_GPIO_ERROR_PIN;
   }
   else {
@@ -413,14 +508,17 @@ static int32_t GPIO_SetPullResistor (ARM_GPIO_Pin_t pin, ARM_GPIO_PULL_RESISTOR 
   return status;
 }
 
-// Set GPIO Event Trigger
+/* Set GPIO Event Trigger */
 static int32_t GPIO_SetEventTrigger (ARM_GPIO_Pin_t pin, ARM_GPIO_EVENT_TRIGGER trigger) {
   GPIO_Type *gpio;
   uint32_t   gpio_num;
   uint32_t   pin_num;
   int32_t    status;
 
-  if (pin >= GPIO_MAX_PINS) {
+  gpio_num = pin >> 3U;
+  pin_num  = pin & 0x7U;
+
+  if (pin_num >= gpio_res[gpio_num].max_pins) {
     status = ARM_GPIO_ERROR_PIN;
   }
   else {
@@ -429,7 +527,7 @@ static int32_t GPIO_SetEventTrigger (ARM_GPIO_Pin_t pin, ARM_GPIO_EVENT_TRIGGER 
     gpio_num = pin >> 3U;
     pin_num  = pin & 0x7U;
 
-    gpio = (GPIO_Type *)GPIO_Base[gpio_num];
+    gpio = (GPIO_Type *)gpio_res[gpio_num].base;
 
     if (trigger == ARM_GPIO_TRIGGER_NONE) {
       /* No trigger, just disable interrupt */
@@ -457,17 +555,18 @@ static int32_t GPIO_SetEventTrigger (ARM_GPIO_Pin_t pin, ARM_GPIO_EVENT_TRIGGER 
   return status;
 }
 
-// Set GPIO Output Level
+/* Set GPIO Output Level */
 static void GPIO_SetOutput (ARM_GPIO_Pin_t pin, uint32_t val) {
   GPIO_Type *gpio;
   uint32_t   gpio_num;
   uint32_t   pin_num;
 
-  if (pin < GPIO_MAX_PINS) {
-    gpio_num = pin >> 3U;
-    pin_num  = pin & 0x7U;
+  gpio_num = pin >> 3U;
+  pin_num  = pin & 0x7U;
 
-    gpio = (GPIO_Type *)GPIO_Base[gpio_num];
+  if (pin_num < gpio_res[gpio_num].max_pins) {
+
+    gpio = (GPIO_Type *)gpio_res[gpio_num].base;
 
     if (val == 0U) {
       gpio_set_value_low(gpio, pin_num);
@@ -477,21 +576,22 @@ static void GPIO_SetOutput (ARM_GPIO_Pin_t pin, uint32_t val) {
   }
 }
 
-// Get GPIO Input Level
+/* Get GPIO Input Level */
 static uint32_t GPIO_GetInput (ARM_GPIO_Pin_t pin) {
   GPIO_Type *gpio;
   uint32_t   gpio_num;
   uint32_t   pin_num;
   uint32_t   val;
 
-  if (pin >= GPIO_MAX_PINS) {
+  gpio_num = pin >> 3U;
+  pin_num  = pin & 0x7U;
+
+  if (pin_num >= gpio_res[gpio_num].max_pins) {
     val = 0U;
   }
   else {
-    gpio_num = pin >> 3U;
-    pin_num  = pin & 0x7U;
 
-    gpio = (GPIO_Type *)GPIO_Base[gpio_num];
+    gpio = (GPIO_Type *)gpio_res[gpio_num].base;
 
     val = gpio_get_value(gpio, pin_num);
   }
@@ -500,7 +600,7 @@ static uint32_t GPIO_GetInput (ARM_GPIO_Pin_t pin) {
 }
 
 
-// GPIO Driver access structure
+/* GPIO Driver access structure */
 ARM_DRIVER_GPIO Driver_GPIO = {
   GPIO_Setup,
   GPIO_SetDirection,
--- Alif_CMSIS/Source/Driver_GPIO_Private.h
+++ Alif_CMSIS/Source/Driver_GPIO_Private.h
@@ -18,12 +18,13 @@
  * $Date:        16. July 2024
  * $Revision:    V1.0
  *
- * Project:      GPIO Driver for Alif Semiconductor Ensemble
+ * Project:      GPIO Driver for Alif Semiconductor
  */
 
 #ifndef DRIVER_GPIO_PRIVATE_H_
 #define DRIVER_GPIO_PRIVATE_H_
 
+#include "RTE_Device.h"
 #include "RTE_Components.h"
 #include CMSIS_device_header
 
@@ -34,25 +35,60 @@ extern "C"
 {
 #endif
 
-// Pin mapping
+/* Pin mapping */
 #define GPIO_PIN(port, pin) ((8U * port) + (pin))
 
+#if (RTE_GPIO0)
 #define GPIO_P0(n)          (  0U + (n))
+#endif
+#if (RTE_GPIO1)
 #define GPIO_P1(n)          (  8U + (n))
+#endif
+#if (RTE_GPIO2)
 #define GPIO_P2(n)          ( 16U + (n))
+#endif
+#if (RTE_GPIO3)
 #define GPIO_P3(n)          ( 24U + (n))
+#endif
+#if (RTE_GPIO4)
 #define GPIO_P4(n)          ( 32U + (n))
+#endif
+#if (RTE_GPIO5)
 #define GPIO_P5(n)          ( 40U + (n))
-#define GPIO_P7(n)          ( 48U + (n))
-#define GPIO_P8(n)          ( 56U + (n))
-#define GPIO_P9(n)          ( 64U + (n))
-#define GPIO_P10(n)         ( 72U + (n))
-#define GPIO_P11(n)         ( 80U + (n))
-#define GPIO_P12(n)         ( 88U + (n))
+#endif
+#if (RTE_GPIO6)
+#define GPIO_P6(n)          ( 48U + (n))
+#endif
+#if (RTE_GPIO7)
+#define GPIO_P7(n)          ( 56U + (n))
+#endif
+#if (RTE_GPIO8)
+#define GPIO_P8(n)          ( 64U + (n))
+#endif
+#if (RTE_GPIO9)
+#define GPIO_P9(n)          ( 72U + (n))
+#endif
+#if (RTE_GPIO10)
+#define GPIO_P10(n)         ( 80U + (n))
+#endif
+#if (RTE_GPIO11)
+#define GPIO_P11(n)         ( 88U + (n))
+#endif
+#if (RTE_GPIO12)
+#define GPIO_P12(n)         ( 96U + (n))
+#endif
+#if (RTE_GPIO13)
 #define GPIO_P13(n)         (104U + (n))
-#define GPIO_P15(n)         (112U + (n))
+#endif
+#if (RTE_GPIO14)
+#define GPIO_P14(n)         (112U + (n))
+#endif
+#if (RTE_LPGPIO)
+#define GPIO_P15(n)         (120U + (n))
+#endif
 
-// GPIOx IRQ Handlers
+/* GPIOx IRQ Handlers */
+#if (RTE_GPIO0)
 extern void GPIO0_IRQ0Handler (void);
 extern void GPIO0_IRQ1Handler (void);
 extern void GPIO0_IRQ2Handler (void);
@@ -61,7 +97,9 @@ extern void GPIO0_IRQ4Handler (void);
 extern void GPIO0_IRQ5Handler (void);
 extern void GPIO0_IRQ6Handler (void);
 extern void GPIO0_IRQ7Handler (void);
+#endif
 
+#if (RTE_GPIO1)
 extern void GPIO1_IRQ0Handler (void);
 extern void GPIO1_IRQ1Handler (void);
 extern void GPIO1_IRQ2Handler (void);
@@ -70,7 +108,9 @@ extern void GPIO1_IRQ4Handler (void);
 extern void GPIO1_IRQ5Handler (void);
 extern void GPIO1_IRQ6Handler (void);
 extern void GPIO1_IRQ7Handler (void);
+#endif
 
+#if (RTE_GPIO2)
 extern void GPIO2_IRQ0Handler (void);
 extern void GPIO2_IRQ1Handler (void);
 extern void GPIO2_IRQ2Handler (void);
@@ -79,7 +119,9 @@ extern void GPIO2_IRQ4Handler (void);
 extern void GPIO2_IRQ5Handler (void);
 extern void GPIO2_IRQ6Handler (void);
 extern void GPIO2_IRQ7Handler (void);
+#endif
 
+#if (RTE_GPIO3)
 extern void GPIO3_IRQ0Handler (void);
 extern void GPIO3_IRQ1Handler (void);
 extern void GPIO3_IRQ2Handler (void);
@@ -88,7 +130,9 @@ extern void GPIO3_IRQ4Handler (void);
 extern void GPIO3_IRQ5Handler (void);
 extern void GPIO3_IRQ6Handler (void);
 extern void GPIO3_IRQ7Handler (void);
+#endif
 
+#if (RTE_GPIO4)
 extern void GPIO4_IRQ0Handler (void);
 extern void GPIO4_IRQ1Handler (void);
 extern void GPIO4_IRQ2Handler (void);
@@ -97,7 +141,9 @@ extern void GPIO4_IRQ4Handler (void);
 extern void GPIO4_IRQ5Handler (void);
 extern void GPIO4_IRQ6Handler (void);
 extern void GPIO4_IRQ7Handler (void);
+#endif
 
+#if (RTE_GPIO5)
 extern void GPIO5_IRQ0Handler (void);
 extern void GPIO5_IRQ1Handler (void);
 extern void GPIO5_IRQ2Handler (void);
@@ -106,7 +152,9 @@ extern void GPIO5_IRQ4Handler (void);
 extern void GPIO5_IRQ5Handler (void);
 extern void GPIO5_IRQ6Handler (void);
 extern void GPIO5_IRQ7Handler (void);
+#endif
 
+#if (RTE_GPIO6)
 extern void GPIO6_IRQ0Handler (void);
 extern void GPIO6_IRQ1Handler (void);
 extern void GPIO6_IRQ2Handler (void);
@@ -115,7 +163,9 @@ extern void GPIO6_IRQ4Handler (void);
 extern void GPIO6_IRQ5Handler (void);
 extern void GPIO6_IRQ6Handler (void);
 extern void GPIO6_IRQ7Handler (void);
+#endif
 
+#if (RTE_GPIO7)
 extern void GPIO7_IRQ0Handler (void);
 extern void GPIO7_IRQ1Handler (void);
 extern void GPIO7_IRQ2Handler (void);
@@ -124,7 +174,9 @@ extern void GPIO7_IRQ4Handler (void);
 extern void GPIO7_IRQ5Handler (void);
 extern void GPIO7_IRQ6Handler (void);
 extern void GPIO7_IRQ7Handler (void);
+#endif
 
+#if (RTE_GPIO8)
 extern void GPIO8_IRQ0Handler (void);
 extern void GPIO8_IRQ1Handler (void);
 extern void GPIO8_IRQ2Handler (void);
@@ -133,7 +185,9 @@ extern void GPIO8_IRQ4Handler (void);
 extern void GPIO8_IRQ5Handler (void);
 extern void GPIO8_IRQ6Handler (void);
 extern void GPIO8_IRQ7Handler (void);
+#endif
 
+#if (RTE_GPIO9)
 extern void GPIO9_IRQ0Handler (void);
 extern void GPIO9_IRQ1Handler (void);
 extern void GPIO9_IRQ2Handler (void);
@@ -142,7 +196,9 @@ extern void GPIO9_IRQ4Handler (void);
 extern void GPIO9_IRQ5Handler (void);
 extern void GPIO9_IRQ6Handler (void);
 extern void GPIO9_IRQ7Handler (void);
+#endif
 
+#if (RTE_GPIO10)
 extern void GPIO10_IRQ0Handler (void);
 extern void GPIO10_IRQ1Handler (void);
 extern void GPIO10_IRQ2Handler (void);
@@ -151,7 +207,9 @@ extern void GPIO10_IRQ4Handler (void);
 extern void GPIO10_IRQ5Handler (void);
 extern void GPIO10_IRQ6Handler (void);
 extern void GPIO10_IRQ7Handler (void);
+#endif
 
+#if (RTE_GPIO11)
 extern void GPIO11_IRQ0Handler (void);
 extern void GPIO11_IRQ1Handler (void);
 extern void GPIO11_IRQ2Handler (void);
@@ -160,7 +218,9 @@ extern void GPIO11_IRQ4Handler (void);
 extern void GPIO11_IRQ5Handler (void);
 extern void GPIO11_IRQ6Handler (void);
 extern void GPIO11_IRQ7Handler (void);
+#endif
 
+#if (RTE_GPIO12)
 extern void GPIO12_IRQ0Handler (void);
 extern void GPIO12_IRQ1Handler (void);
 extern void GPIO12_IRQ2Handler (void);
@@ -169,7 +229,9 @@ extern void GPIO12_IRQ4Handler (void);
 extern void GPIO12_IRQ5Handler (void);
 extern void GPIO12_IRQ6Handler (void);
 extern void GPIO12_IRQ7Handler (void);
+#endif
 
+#if (RTE_GPIO13)
 extern void GPIO13_IRQ0Handler (void);
 extern void GPIO13_IRQ1Handler (void);
 extern void GPIO13_IRQ2Handler (void);
@@ -178,7 +240,9 @@ extern void GPIO13_IRQ4Handler (void);
 extern void GPIO13_IRQ5Handler (void);
 extern void GPIO13_IRQ6Handler (void);
 extern void GPIO13_IRQ7Handler (void);
+#endif
 
+#if (RTE_GPIO14)
 extern void GPIO14_IRQ0Handler (void);
 extern void GPIO14_IRQ1Handler (void);
 extern void GPIO14_IRQ2Handler (void);
@@ -187,17 +251,22 @@ extern void GPIO14_IRQ4Handler (void);
 extern void GPIO14_IRQ5Handler (void);
 extern void GPIO14_IRQ6Handler (void);
 extern void GPIO14_IRQ7Handler (void);
+#endif
 
+#if (RTE_LPGPIO)
 extern void LPGPIO_IRQ0Handler (void);
 extern void LPGPIO_IRQ1Handler (void);
+#if (SOC_FEAT_LPGPIO_HAS_PIN2_7)
 extern void LPGPIO_IRQ2Handler (void);
 extern void LPGPIO_IRQ3Handler (void);
 extern void LPGPIO_IRQ4Handler (void);
 extern void LPGPIO_IRQ5Handler (void);
 extern void LPGPIO_IRQ6Handler (void);
 extern void LPGPIO_IRQ7Handler (void);
+#endif
+#endif
 
-// GPIO Driver access structure
+/* GPIO Driver access structure */
 extern ARM_DRIVER_GPIO Driver_GPIO;
 
 #ifdef  __cplusplus
